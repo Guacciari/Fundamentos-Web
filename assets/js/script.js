1d047ec2964785799c8bb5563c6fc8e5ae63dc6a
Guacciari <127262251+Guacciari@users.noreply.github.com>
2023-05-19 09:13:15 -0300

    js

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// tipos de declaração de variaveis
+// var / let / const
+
+// var x = 'x é meu nome' //var pode ser alterado e sempre vai ter o escopo global
+// let y = 'meu nome é y' //let pode ser alterado, e tem o escopo local de onde foi criado
+// const numero = 2 //const não pode ser alterado depois de declarado, e tem escopo local, assim como o let
+
+// alert(y)
+
+// Capturando elementos pela DOM
+// ao usar o "document.", ganhamos acesso no HTML por inteiro, e podemos buscar os elementos por ID ou Classe, por exemplo
+const nome = document.getElementById('nome')
+const email = document.querySelector('#email')
+// console.log(nome);git 
+
+// function olaMundo() {
+//   alert('clicou no botão')
+// }
+
+function validaNome() {
+  let txtNome = document.querySelector('#txtNome')
+  console.log(nome.value);
+  if(nome.value.length < 2) {
+    txtNome.innerHTML = 'Nome invalido'
+    txtNome.style.color = 'red'
+    nome.style.border='3px solid red'
+  } else {
+    txtNome.innerHTML = 'Nome valido'
+    txtNome.style.color = 'green'
+    nome.style.border='3px solid green'
+  }
+  
+  function validaEmail () {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){ 
+        txtEmail.innerHTML = 'E-mail inválido'
+        email.style.color = 'red'
+        email.style.border='3px solid red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido'
+        email.style.color = 'green'
+        email.style.border='3px solid green'
+        emailOk = true
+    }
+    }
+}
